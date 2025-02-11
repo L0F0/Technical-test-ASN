357015b355a903f8287df81b37436229e42a42ba
Yaniss <yaniss.pheron@epitech.eu>
2025-02-11 20:24:49 +0100

    fix insertion to db

--- server/src/equipment/equipment.controller.ts
+++ server/src/equipment/equipment.controller.ts
@@ -1,18 +1,19 @@
 import { Controller, Post, Body, Get, Param } from '@nestjs/common';
 import { EquipmentService } from './equipment.service';
 import { EquipmentCategoryDto } from './dto/equipment-category.dto';
-import { EquipmentCategory } from './interfaces/equipment-category.interface';
 
 @Controller('api/equipment')
 export class EquipmentController {
-  constructor(private readonly equipmentService: EquipmentService) {}
+  constructor(private equipmentService: EquipmentService) {}
 
   @Post('kraken')
-  async createOrUpdate(@Body() equipmentCategoryDto: EquipmentCategoryDto[]) {
-    for (let i = 0; i < equipmentCategoryDto.length; i++) {
-      const currentEquipment = equipmentCategoryDto[i];
+  async createOrUpdate(
+    @Body() equipmentCategoryDto: { equipment: EquipmentCategoryDto[] },
+  ) {
+    for (let i = 0; i < equipmentCategoryDto.equipment.length; i++) {
+      const currentEquipment = equipmentCategoryDto.equipment[i];
       await this.equipmentService.updateEquipment(currentEquipment);
     }
-    return;
+    return 'ok';
   }
 }
--- server/src/equipment/schemas/equipment-category.schema.ts
+++ server/src/equipment/schemas/equipment-category.schema.ts
@@ -2,7 +2,7 @@ import { Schema } from 'mongoose';
 
 export const EquipmentCategorySchema = new Schema({
   name: { type: String, required: true },
-  updated_at: { type: Date, default: Date.now },
+  updated_at: { type: Date, required: true },
   prices: { type: [Number], required: true },
   rate: { type: Number, required: true },
   category: { type: String, enum: ['product', 'equipment'], required: true },
